794486ed39e9477ce63fbf374796e06e418a9c23
Sean Gray <seangray.dev@gmail.com>
2023-02-17 23:53:13 +0000

    add input.js + refactor code

---
+++ input.js
@@ -0,0 +1,20 @@
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (key) {
+    // check for ctrl + c input
+    if (key === "\u0003") {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,28 +1,7 @@
 const { connect } = require("./client");
-
-const connection = connect();
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (key) {
-    // check for ctrl + c input
-    if (key === "\u0003") {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-
-setupInput();
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
+
+connect();
+setupInput();
