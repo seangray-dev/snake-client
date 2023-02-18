c27dbc09ae5d9c14a30f2a50641b2c8bedb1f0fb
Sean Gray <seangray.dev@gmail.com>
2023-02-18 00:28:34 +0000

    allow wasd movement

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,6 +13,14 @@ const setupInput = function () {
     // check for ctrl + c input
     if (key === "\u0003") {
       process.exit();
+    } else if (key === "w") {
+      connection.write("Move: up"); // send "Move: up" message to the server
+    } else if (key === "a") {
+      connection.write("Move: left"); // send "Move: left" message to the server
+    } else if (key === "s") {
+      connection.write("Move: down"); // send "Move: down" message to the server
+    } else if (key === "d") {
+      connection.write("Move: right"); // send "Move: right" message to the server
     }
   };
 
--- play.js
+++ play.js
@@ -3,5 +3,5 @@ const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 
-connect();
-setupInput();
+const conn = connect(); // store conn object returned by connect
+setupInput(conn); // pass conn object to setupInput
